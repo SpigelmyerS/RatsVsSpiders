e40f23bec64a56071ba335ac628ec3844e68c35c
Suzariah <Suz2478@gmail.com>
2016-02-22 15:26:55 -0500

    main + rat+ spider+frame +panel

--- RatsVsSpiders/src/Frame.java
+++ RatsVsSpiders/src/Frame.java
@@ -1,3 +1,7 @@
+
+import javax.swing.*;
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -8,6 +12,16 @@
  *
  * @author Suzanne
  */
-public class Frame {
+public class Frame extends JFrame{
     
+    Frame(){
+        
+    
+     super ("Test");
+        MainMenuPanel panel = new MainMenuPanel();
+        getContentPane().add(panel);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize (900, 750);
+        setVisible(true);    
+    }
 }
