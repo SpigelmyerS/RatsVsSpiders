f727824454b3f397f5c2b014e4aaefa991918773
JasonHitchings <jhitch1995@gmail.com>
2016-02-25 16:00:08 -0500

    MainMenuFrame added with quit functionality. Will add start once the other classes are completed

---
+++ RatsVsSpiders/src/MainMenuFrame.java
@@ -0,0 +1,84 @@
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Jason
+ */
+public class MainMenuFrame extends JFrame {
+
+    private static final int FRAME_WIDTH = 450;
+    private static final int FRAME_HEIGHT = 450;
+    private JButton startButton;
+    private JButton quitButton;
+    private JLabel startLabel;
+    private JLabel quitLabel;
+    private JTextField RatsVSpidersField;
+
+    public MainMenuFrame() {
+        this.createComponents();
+        this.createPanel();
+        this.setSize(FRAME_WIDTH, FRAME_HEIGHT);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+    }
+
+    private void createComponents() {
+        this.createButtons();
+        this.createTextField();
+    }
+
+    private void createButtons() {
+        class StartQuitListener implements ActionListener {
+
+            private String action;
+
+            public StartQuitListener(String action) {
+                this.action = action.toLowerCase();
+            }
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(action.equals("start")){
+                    
+                }
+                if(action.equals("quit")){
+                System.exit(0);
+            }
+            }
+
+           
+            
+        }
+        startButton = new JButton("Start");
+        ActionListener startListener = new StartQuitListener("sale");
+        startButton.addActionListener(startListener);
+        quitButton = new JButton("Quit");
+        ActionListener quitListener = new StartQuitListener("Quit");
+        quitButton.addActionListener(quitListener);
+        
+    }
+
+    private void createTextField() {
+        final int FIELD_WIDTH = 9;
+        RatsVSpidersField = new JTextField(FIELD_WIDTH);
+        RatsVSpidersField.setText("RATS VS SPIDERS!");
+    }
+
+    private void createPanel() {
+        JPanel panel = new JPanel();
+        panel.add(this.RatsVSpidersField);
+        panel.add(this.startButton);
+        panel.add(this.startLabel);
+        panel.add(this.quitButton);
+        panel.add(this.quitLabel);
+        this.add(panel);
+    }
+}
