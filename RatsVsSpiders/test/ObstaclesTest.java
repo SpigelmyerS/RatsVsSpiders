19e93d5fd3da100760359fb11827c777db4ce7c8
Matthew <Matthew@R2-D2.home>
2016-03-04 23:04:26 -0500

    Altered Obstacles and mainTest and removed Fail instances.

--- RatsVsSpiders/test/ObstaclesTest.java
+++ RatsVsSpiders/test/ObstaclesTest.java
@@ -46,8 +46,6 @@ public class ObstaclesTest {
         Graphics g = null;
         Obstacles instance = null;
         instance.Draw(g);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
     
 }
